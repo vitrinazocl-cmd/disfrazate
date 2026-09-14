a4f4f1efd26734c8845f87d025b68662c51e32b2
Juan Carlos Mena Varas <ext_jmena@falabella.com>
2026-09-14 11:04:51 -0300

    Sincronizacion de productos DIS-0119 a DIS-0185 en el catalogo

--- catalogo.js
+++ catalogo.js
@@ -1414,5 +1414,809 @@ const catalogoProductos = [
             "6"
         ],
         "isOffer": true
+    },
+    {
+        "id": "DIS-0119",
+        "name": "Mario",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0119.jpg",
+        "highResImage": "DIS-0119.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0120",
+        "name": "Esqueleto Moicano",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0120.jpg",
+        "highResImage": "DIS-0120.jpg",
+        "sizes": [
+            "8"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0121",
+        "name": "Capitan America",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0121.jpg",
+        "highResImage": "DIS-0121.jpg",
+        "sizes": [
+            "4"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0122",
+        "name": "Spiderman",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0122.jpg",
+        "highResImage": "DIS-0122.jpg",
+        "sizes": [
+            "10"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0123",
+        "name": "Spiderman",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0123.jpg",
+        "highResImage": "DIS-0123.jpg",
+        "sizes": [
+            "4"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0124",
+        "name": "Spiderman Negro",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0124.jpg",
+        "highResImage": "DIS-0124.jpg",
+        "sizes": [
+            "10"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0125",
+        "name": "Master Chief",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0125.jpg",
+        "highResImage": "DIS-0125.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0126",
+        "name": "Jake Pirata",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0126.jpg",
+        "highResImage": "DIS-0126.jpg",
+        "sizes": [
+            "4"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0127",
+        "name": "Spiderman",
+        "price": 10000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0127.jpg",
+        "highResImage": "DIS-0127.jpg",
+        "sizes": [
+            "10"
+        ],
+        "isOffer": false
+    },
+    {
+        "id": "DIS-0128",
+        "name": "Tunica",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0128.jpg",
+        "highResImage": "DIS-0128.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0129",
+        "name": "Traje Amarillos",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0129.jpg",
+        "highResImage": "DIS-0129.jpg",
+        "sizes": [
+            "12"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0130",
+        "name": "Batman",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0130.jpg",
+        "highResImage": "DIS-0130.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0131",
+        "name": "Transformers",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0131.jpg",
+        "highResImage": "DIS-0131.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0132",
+        "name": "Star Trek",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0132.jpg",
+        "highResImage": "DIS-0132.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0133",
+        "name": "Spiderman Negro",
+        "price": 8000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0133.jpg",
+        "highResImage": "DIS-0133.jpg",
+        "sizes": [
+            "5"
+        ],
+        "isOffer": false
+    },
+    {
+        "id": "DIS-0134",
+        "name": "Superman",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0134.jpg",
+        "highResImage": "DIS-0134.jpg",
+        "sizes": [
+            "2"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0135",
+        "name": "Flash",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0135.jpg",
+        "highResImage": "DIS-0135.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0136",
+        "name": "Spiderman Negro",
+        "price": 10000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0136.jpg",
+        "highResImage": "DIS-0136.jpg",
+        "sizes": [
+            "8"
+        ],
+        "isOffer": false
+    },
+    {
+        "id": "DIS-0137",
+        "name": "Spiderman",
+        "price": 8000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0137.jpg",
+        "highResImage": "DIS-0137.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": false
+    },
+    {
+        "id": "DIS-0138",
+        "name": "Extraño Mundo de Jack",
+        "price": 10000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0138.jpg",
+        "highResImage": "DIS-0138.jpg",
+        "sizes": [
+            "4"
+        ],
+        "isOffer": false
+    },
+    {
+        "id": "DIS-0139",
+        "name": "Wolverine",
+        "price": 10000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0139.jpg",
+        "highResImage": "DIS-0139.jpg",
+        "sizes": [
+            "8"
+        ],
+        "isOffer": false
+    },
+    {
+        "id": "DIS-0140",
+        "name": "Iroman",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0140.jpg",
+        "highResImage": "DIS-0140.jpg",
+        "sizes": [
+            "7"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0141",
+        "name": "Spiderman",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0141.jpg",
+        "highResImage": "DIS-0141.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0142",
+        "name": "Hulk",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0142.jpg",
+        "highResImage": "DIS-0142.jpg",
+        "sizes": [
+            "8"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0143",
+        "name": "Spiderman",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0143.jpg",
+        "highResImage": "DIS-0143.jpg",
+        "sizes": [
+            "3"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0144",
+        "name": "Transformers",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0144.jpg",
+        "highResImage": "DIS-0144.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0145",
+        "name": "Pantera Advengers",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0145.jpg",
+        "highResImage": "DIS-0145.jpg",
+        "sizes": [
+            "4"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0146",
+        "name": "Mortal Comball",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0146.jpg",
+        "highResImage": "DIS-0146.jpg",
+        "sizes": [
+            "8"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0147",
+        "name": "Minecraf",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0147.jpg",
+        "highResImage": "DIS-0147.jpg",
+        "sizes": [
+            "12"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0148",
+        "name": "Mariposa",
+        "price": 7000,
+        "category": "NIÑA",
+        "image": "catalogo final/DIS-0148.jpg",
+        "highResImage": "DIS-0148.jpg",
+        "sizes": [
+            "12"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0149",
+        "name": "Ant-man",
+        "price": 10000,
+        "category": "HOMBRE",
+        "image": "catalogo final/DIS-0149.jpg",
+        "highResImage": "DIS-0149.jpg",
+        "sizes": [
+            "S"
+        ],
+        "isOffer": false
+    },
+    {
+        "id": "DIS-0150",
+        "name": "Scary Movie Gris",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0150.jpg",
+        "highResImage": "DIS-0150.jpg",
+        "sizes": [
+            "8"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0151",
+        "name": "Spidey",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0151.jpg",
+        "highResImage": "DIS-0151.jpg",
+        "sizes": [
+            "2"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0152",
+        "name": "Vestido Gris",
+        "price": 7000,
+        "category": "NIÑA",
+        "image": "catalogo final/DIS-0152.jpg",
+        "highResImage": "DIS-0152.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0153",
+        "name": "Pantera Advengers",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0153.jpg",
+        "highResImage": "DIS-0153.jpg",
+        "sizes": [
+            "12"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0154",
+        "name": "Scary Movie Black",
+        "price": 8000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0154.jpg",
+        "highResImage": "DIS-0154.jpg",
+        "sizes": [
+            "10"
+        ],
+        "isOffer": false
+    },
+    {
+        "id": "DIS-0155",
+        "name": "Power Rangers",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0155.jpg",
+        "highResImage": "DIS-0155.jpg",
+        "sizes": [
+            "8"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0156",
+        "name": "Heroes Pijama",
+        "price": 4000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0156.jpg",
+        "highResImage": "DIS-0156.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0157",
+        "name": "Huesito",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0157.jpg",
+        "highResImage": "DIS-0157.jpg",
+        "sizes": [
+            "5"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0158",
+        "name": "Abuelo",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0158.jpg",
+        "highResImage": "DIS-0158.jpg",
+        "sizes": [
+            "4"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0159",
+        "name": "Mandalorian",
+        "price": 6000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0159.jpg",
+        "highResImage": "DIS-0159.jpg",
+        "sizes": [
+            "5"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0160",
+        "name": "Ana Frozen",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0160.jpg",
+        "highResImage": "DIS-0160.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0161",
+        "name": "Batman",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0161.jpg",
+        "highResImage": "DIS-0161.jpg",
+        "sizes": [
+            "5"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0162",
+        "name": "Aliens",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0162.jpg",
+        "highResImage": "DIS-0162.jpg",
+        "sizes": [
+            "8"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0163",
+        "name": "Mariposa",
+        "price": 7000,
+        "category": "NIÑA",
+        "image": "catalogo final/DIS-0163.jpg",
+        "highResImage": "DIS-0163.jpg",
+        "sizes": [
+            "7"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0164",
+        "name": "Alta Montana",
+        "price": 4000,
+        "category": "NIÑA",
+        "image": "catalogo final/DIS-0164.jpg",
+        "highResImage": "DIS-0164.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0165",
+        "name": "Batman",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0165.jpg",
+        "highResImage": "DIS-0165.jpg",
+        "sizes": [
+            "8"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0166",
+        "name": "Spiderman Negro",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0166.jpg",
+        "highResImage": "DIS-0166.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0167",
+        "name": "Capitan America",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0167.jpg",
+        "highResImage": "DIS-0167.jpg",
+        "sizes": [
+            "3"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0168",
+        "name": "Fornite",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0168.jpg",
+        "highResImage": "DIS-0168.jpg",
+        "sizes": [
+            "8"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0169",
+        "name": "Esqueleto",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0169.jpg",
+        "highResImage": "DIS-0169.jpg",
+        "sizes": [
+            "10"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0170",
+        "name": "Capitan America",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0170.jpg",
+        "highResImage": "DIS-0170.jpg",
+        "sizes": [
+            "2"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0171",
+        "name": "Power Rangers",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0171.jpg",
+        "highResImage": "DIS-0171.jpg",
+        "sizes": [
+            "7"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0172",
+        "name": "Starwars",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0172.jpg",
+        "highResImage": "DIS-0172.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0173",
+        "name": "Superman",
+        "price": 6000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0173.jpg",
+        "highResImage": "DIS-0173.jpg",
+        "sizes": [
+            "3"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0174",
+        "name": "Spiderman",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0174.jpg",
+        "highResImage": "DIS-0174.jpg",
+        "sizes": [
+            "5"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0175",
+        "name": "Elsa Frozen",
+        "price": 4000,
+        "category": "NIÑA",
+        "image": "catalogo final/DIS-0175.jpg",
+        "highResImage": "DIS-0175.jpg",
+        "sizes": [
+            "3"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0176",
+        "name": "Bruja",
+        "price": 10000,
+        "category": "NIÑA",
+        "image": "catalogo final/DIS-0176.jpg",
+        "highResImage": "DIS-0176.jpg",
+        "sizes": [
+            "10"
+        ],
+        "isOffer": false
+    },
+    {
+        "id": "DIS-0177",
+        "name": "Spiderman",
+        "price": 8000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0177.jpg",
+        "highResImage": "DIS-0177.jpg",
+        "sizes": [
+            "7"
+        ],
+        "isOffer": false
+    },
+    {
+        "id": "DIS-0178",
+        "name": "Capitan America",
+        "price": 6000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0178.jpg",
+        "highResImage": "DIS-0178.jpg",
+        "sizes": [
+            "3"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0179",
+        "name": "Spiderman",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0179.jpg",
+        "highResImage": "DIS-0179.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0180",
+        "name": "Sonic Amarillo",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0180.jpg",
+        "highResImage": "DIS-0180.jpg",
+        "sizes": [
+            "6"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0181",
+        "name": "Zombie Alien",
+        "price": 6000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0181.jpg",
+        "highResImage": "DIS-0181.jpg",
+        "sizes": [
+            "10"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0182",
+        "name": "Ninja",
+        "price": 5000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0182.jpg",
+        "highResImage": "DIS-0182.jpg",
+        "sizes": [
+            "3"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0183",
+        "name": "Entrenando a Tu Dragon",
+        "price": 7000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0183.jpg",
+        "highResImage": "DIS-0183.jpg",
+        "sizes": [
+            "8"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0184",
+        "name": "Princesa Pink",
+        "price": 6000,
+        "category": "NIÑA",
+        "image": "catalogo final/DIS-0184.jpg",
+        "highResImage": "DIS-0184.jpg",
+        "sizes": [
+            "7"
+        ],
+        "isOffer": true
+    },
+    {
+        "id": "DIS-0185",
+        "name": "Ezra Star Wars",
+        "price": 6000,
+        "category": "NIÑO",
+        "image": "catalogo final/DIS-0185.jpg",
+        "highResImage": "DIS-0185.jpg",
+        "sizes": [
+            "7"
+        ],
+        "isOffer": true
     }
 ];
