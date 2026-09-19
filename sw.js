c0af4e9ba7e283fe46c625772c1aead7fd5724f6
Juan Carlos Mena Varas <ext_jmena@falabella.com>
2026-09-19 00:13:01 -0300

    feat: Listener global de captura directa en index.html para ampliado instantaneo de fotos a pantalla completa en cualquier dispositivo

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'disfrazate-cache-v15';
+const CACHE_NAME = 'disfrazate-cache-v16';
 
 const PRECACHE_ASSETS = [
     './',
     './index.html',
-    './catalogo.js?v=10.0',
-    './script.js?v=10.0',
+    './catalogo.js?v=11.0',
+    './script.js?v=11.0',
     './style.css',
     './logo_disfrazate_tech.jpg'
 ];
