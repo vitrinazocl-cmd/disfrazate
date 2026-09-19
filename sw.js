0bd8ea453029be6aae3b1b377c2e13d7e07f7efc
Juan Carlos Mena Varas <ext_jmena@falabella.com>
2026-09-18 23:39:36 -0300

    fix: Preload catalogo.js in head tag v6.0 and update sw.js v11 to guarantee instant script execution

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'disfrazate-cache-v10';
+const CACHE_NAME = 'disfrazate-cache-v11';
 
 const PRECACHE_ASSETS = [
     './',
     './index.html',
-    './catalogo.js?v=5.0',
-    './script.js?v=5.0',
+    './catalogo.js?v=6.0',
+    './script.js?v=6.0',
     './style.css',
     './logo_disfrazate_tech.jpg'
 ];
